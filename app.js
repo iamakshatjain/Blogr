25613d3882edb9e655e875eedb185a347f59a752
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2018-12-15 02:06:31 +0530

    CRUD Routes and styling

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require("express");
 var app =  express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
 mongoose.connect("mongodb://localhost/blogApp",{ useNewUrlParser: true });//db-blogApp
+var expressSanitizer = require("express-sanitizer");
 
 //APP CONFIG
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 
 //MONGOOSE/MODEL CONFIG
@@ -46,7 +50,7 @@ app.post("/blogs",function(req,res){
 	var newblog = {
 		title:req.body.title,
 		image:req.body.image,
-		body:req.body.body
+		body:req.sanitize(req.body.body)
 	};
 
 	blog.create(newblog,function(err,blog){
@@ -58,6 +62,56 @@ app.post("/blogs",function(req,res){
 		}
 
 	});
+});
+
+//SHOW
+app.get("/blogs/:id",function(req,res){
+	var id = req.params.id;
+	blog.findById(id,function(err,post){
+		if(err)
+			console.log("Error line#68");
+		else
+			res.render("show",{blog:post});
+	});
+	
+});
+
+//EDIT
+app.get("/blogs/:id/edit",function(req,res){
+	var id = req.params.id;
+	blog.findById(id,function(err,editBlog){
+		if(err)
+			console.log("Error line#80");
+		else
+			res.render("edit",{blog:editBlog});
+	});
+});
+
+//UPDATE
+app.put("/blogs/:id",function(req,res){
+	var updateBlog={};
+	updateBlog.title=req.body.title;
+	updateBlog.image=req.body.image;
+	updateBlog.body=req.sanitize(req.body.body);
+	// console.log(updateBlog);
+	blog.findByIdAndUpdate(req.params.id,updateBlog,function(err,updatedBlog){
+		if(err)
+			console.log("error line#92");
+		else{
+			
+			res.redirect("/blogs/"+updatedBlog._id)
+			}
+	});
+});
+
+//DELETE
+app.delete("/blogs/:id",function(req,res){
+	blog.findByIdAndRemove(req.params.id,function(err){
+		if(err)
+			console.log("error line#109");
+		else
+			res.redirect("/");
+	})
 });
 
 app.listen(process.env.PORT || 3000,process.env.IP,function(){
